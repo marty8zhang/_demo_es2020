3c24d7544b2c8b6acd40c449eb60bb4d9a9c7175
Marty Zhang <marty8zhang@gmail.com>
2020-08-14 17:18:38 +1000

    Add the code example(s) for "Optional Chaining Operator".

---
+++ src/optional-chaining-operator.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const object = {
+  a1: {
+    a2: {
+      a3: 'okay',
+    },
+  },
+  b1: {
+    b2: null,
+  },
+}
+
+console.log(object.a1.a2.a3) // okay.
+// console.log(object.b1.b2.b3) // TypeError.
+// console.log(object.c1.c2) // TypeError.
+
+console.log(object?.a1?.a2?.a3) // okay.
+console.log(object.b1.b2?.b3) // undefined.
+console.log(object?.c1?.c2) // undefined.
