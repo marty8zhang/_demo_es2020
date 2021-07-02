a748ed41497f6104f97b764a228db51a999cf897
Marty Zhang <marty8zhang@gmail.com>
2021-07-02 18:17:41 +1000

    Improve the code example(s) for "Optional Chaining Operator".

--- src/optional-chaining-operator.js
+++ src/optional-chaining-operator.js
@@ -15,6 +15,9 @@ console.log(object.a1.a2.a3) // okay.
 // console.log(object.b1.b2.b3) // TypeError.
 // console.log(object.c1.c2) // TypeError.
 
-console.log(object?.a1?.a2?.a3) // okay.
+console.log(object.a1?.a2?.a3) // okay.
 console.log(object.b1.b2?.b3) // undefined.
-console.log(object?.c1?.c2) // undefined.
+console.log(object.c1?.c2) // undefined.
+console.log(object.nonExistingMethod?.()) // undefined.
+
+// console.log(nonExistingObject?.test) // ReferenceError.
